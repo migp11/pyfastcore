4d9ea897715c69ce65155d8ffded74e9335068e6
mponce <miguelponcedeleon@gmail.com>
2020-02-26 01:01:20 +0100

    working setup.py

--- setup.py
+++ setup.py
@@ -1,74 +1,22 @@
-"""build setup for fastcore
-"""
+import setuptools
 
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
-import versioneer
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name='fastcore',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description='Fast reconstruction of compact context-specific metabolic network models.',
+setuptools.setup(
+    name="fastcore", # Replace with your own username
+    version="0.0.1",
+    author="migp11",
+    author_email="miguel.ponce@gbsc.es",
+    description="A python implementataio fo the context-specific metabolic model extraction ethods com Vlassis et al. 2014",
     long_description=long_description,
-    url='https://github.com/migp11/fastcore',
-    author='Miguel Ponce de Leon',
-    author_email='miguelponcedeleon@gmail.com.com',
-    license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    long_description_content_type="text/markdown",
+    url="https://github.com/migp11/fastcore",
+    packages=setuptools.find_packages(),
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-
-    # What does your project relate to?
-    keywords='biology metabolism reconstruction cobra',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['doc', 'test']),
-
-    package_data={'fastcore': [path.join("data", "*")]},
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['cobra>=0.6.0a2', 'numpy>=1.10.0'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    }
+    python_requires='>=3.6',
 )
