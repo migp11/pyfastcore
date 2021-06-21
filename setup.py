d9c2f2b84ef7c25502ed6a5a7f82571a87edaf7a
mponce <miguel.ponce@bsc.es>
2021-06-21 18:35:34 +0200

    preparing package for pip

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fastcore", # Replace with your own username
+    name="fastcore",
     version="0.0.1",
-    author="migp11",
+    author="Miguel Ponce de Leon",
     author_email="miguel.ponce@gbsc.es",
-    description="A python implementataio fo the context-specific metabolic model extraction ethods com Vlassis et al. 2014",
+    description="A python-based implementation for the context-specific metabolic model extraction methods from Vlassis et al. 2014",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/migp11/fastcore",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('.'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=install_requires
 )
